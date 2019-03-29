2f54091fc08881dd973d4b99f67147c847c40755
tonyle <tonyle@smu.edu.sg>
2019-03-29 16:00:35 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
+
+def read_md(f):
+    return open(f, 'r').read()
+
+
 setup(
     name='py-helloworld',
-    version='0.0.1',
-    description='a pip-installable package example',
+    version='0.0.2',
+    description='Test python package',
+    long_description=read_md('README.md'),
     license='MIT',
     packages=['helloworld'],
     author='Tony LE',
     author_email='lpthong90@gmail.com',
     keywords=['helloworld'],
-    url='https://github.com/ThongLe/pyhelloworld'
+    url='https://github.com/ThongLe/pyhelloworld',
+    classifiers=[
+        'Programming Language :: Python :: 2.7'
+    ]
 )
