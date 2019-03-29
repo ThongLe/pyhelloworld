55b9a7e28b50e544050353ccb81258b2a05d679b
tonyle <tonyle@smu.edu.sg>
2019-03-29 15:30:59 +0800

    initialize

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py-helloworld',
+    version='0.0.1',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=['helloworld'],
+    author='Tony LE',
+    author_email='lpthong90@gmail.com',
+    keywords=['helloworld'],
+    url='https://github.com/ThongLe/pyhelloworld'
+)
